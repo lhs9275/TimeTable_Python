ba0211bfbabfd535b50e0d422a6c283eda4b6213
my <hslee9275@gmail.com>
2024-04-26 17:29:58 +0900

    버그 픽스 정확도 향상

--- table.py
+++ table.py
@@ -41,7 +41,7 @@ for file_path in file_paths:
         merge = []
         merges =[]
         sign_text = "\n\n 사용자      ❨서명❩ \n\n관리자      ❨서명❩"
-        other_contry = "\n\n 관외사용자    ❨서명❩ \n\n관리자      ❨서명❩"
+        other_contry = "(관외)\n\n 관외사용자    ❨서명❩ \n\n관리자      ❨서명❩"
 
 
         #조건 리스트
@@ -138,7 +138,7 @@ for file_path in file_paths:
                 
                 #일반 예약
                 if (df_data['시설명'] == desired_facility_list[change_colums]).any():
-                    condition = (df_data['시설명'] == desired_facility_list[change_colums]) & (df_data['예약시간'] == desired_reservation_time_list[i])& (df_data['예약상태'].isin(desired_reservation_status_list))&(~df_data['추가금액'])
+                    condition = (df_data['시설명'] == desired_facility_list[change_colums]) & (df_data['예약시간'] == desired_reservation_time_list[i])& (df_data['예약상태'].isin(desired_reservation_status_list))&(df_data['추가금액']==0)
 
                     if condition.any():
                         # 조건을 만족하면 해당 행의 인덱스인 '예약회원'을 출력
@@ -316,7 +316,7 @@ for file_path in file_paths:
                             specific_cells.append((index_value, column_name))
                             
                     if(df_data['시설명'] == desired_facility_list[change_colums]).any():
-                        condition = (df_data['시설명'] == desired_facility_list[change_colums]) & (df_data['예약시간'] == desired_reservation_time_list_4[k])& (df_data['예약상태'].isin(desired_reservation_status_list))&(~df_data['추가금액'])    
+                        condition = (df_data['시설명'] == desired_facility_list[change_colums]) & (df_data['예약시간'] == desired_reservation_time_list_4[k])& (df_data['예약상태'].isin(desired_reservation_status_list))&(df_data['추가금액']==0)    
 
                         if condition.any():
                             # 조건을 만족하면 해당 행의 인덱스인 '예약회원'을 출력
@@ -348,7 +348,7 @@ for file_path in file_paths:
                     if day_of_week_number==6 or 5:
                        
                         if (df_data['시설명'] == desired_facility_list[change_colums]).any():
-                            condition = (df_data['시설명'] == desired_facility_list[change_colums]) & (df_data['예약시간'] == desired_reservation_time_list_4[k])& (df_data['예약상태'].isin(desired_reservation_status_list))&(~df_data['추가금액']==0)&(df_data['할인금액']==0)&(df_data['결제금액'].isin(no_certifiacte_people_weekend))
+                            condition = (df_data['시설명'] == desired_facility_list[change_colums]) & (df_data['예약시간'] == desired_reservation_time_list_4[k])& (df_data['예약상태'].isin(desired_reservation_status_list))&(df_data['추가금액']==0)&(df_data['할인금액']==0)&(df_data['결제금액'].isin(no_certifiacte_people_weekend))
                             
                             if condition.any():
                                 reserved_member = condition[condition].index[0]
